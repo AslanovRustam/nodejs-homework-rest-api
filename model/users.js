4cb9999731f37ce2f99f36a8068be09c80c97be3
Aslanov Rustam <aslanovrg@gmail.com>
2021-03-21 22:08:28 +0200

    add email generator

--- model/users.js
+++ model/users.js
@@ -8,7 +8,7 @@ const findById = async (id) => {
   return await User.findOne({ _id: id });
 };
 
-const create = async ({ email, password }) => {
+const create = async ({ email, password, verify, verifyToken }) => {
   const user = new User({ email, password });
   return await user.save();
 };
@@ -21,10 +21,20 @@ const updateAvatar = async (id, avatar, imgIdCloud) => {
   return await User.updateOne({ _id: id }, { avatar, imgIdCloud });
 };
 
+const findByVerifyToken = async (verifyToken) => {
+  return await User.findOne({ verifyToken });
+};
+
+const updateVerifyToken = async (id, verify, verifyToken) => {
+  return await User.findOneAndUpdate({ _id: id }, { verify, verifyToken }); // [1]
+};
+
 module.exports = {
   findByEmail,
   findById,
   create,
   updateToken,
   updateAvatar,
+  findByVerifyToken,
+  updateVerifyToken,
 };
